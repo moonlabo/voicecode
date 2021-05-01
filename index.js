d569247e189d67e61f90267ddd593a16d303fcef
MoonBot <60772837+moonxibot@users.noreply.github.com>
2021-05-01 19:56:39 +0900

    기본 설정
    변수 기능을 포함한 기본 설정입니다.

---
+++ index.js
@@ -0,0 +1,53 @@
+const variable = {}
+
+function numfy(val) {
+    const numval = Number(val)
+    if (isNaN(numval)) {return val;} //if string
+    else {return numval}
+}
+
+// 브라우저 지원 여부 확인
+if (!('webkitSpeechRecognition' in window)) {
+    alert("VoiceCode is only supported in CHrome for Desktop and Android only.")
+} else {
+    const recognition = new window.webkitSpeechRecognition;
+
+    const status = document.getElementById("status");
+
+    function syntax(code) {
+        words = code.split(" ");
+
+        //변수
+        if (words[0] == "변수") {
+            if (words.length < 4) {
+                return "변수 등록에 알맞지 않은 문법이 사용되었습니다.";
+            } else {
+                const key = String(words.slice(1, words.indexOf("정의")).join(""));
+                const val = numfy(words.slice(words.indexOf("정의")+1, words.length).join(" "));
+
+                variable[key] = val;
+                return true;
+            }
+        }
+    }
+
+    // Start btn click event
+    document.getElementById("start").addEventListener("click", () => {
+        status.innerText = "Listening...";
+        recognition.start()
+    })
+
+    // Stop btn click event
+    document.getElementById("stop").addEventListener("click", () => {
+        status.innerText = "Not Listening...";
+        recognition.stop()
+    });
+
+    // Result event
+    recognition.addEventListener("result", (event) => {
+        const { results, resultIndex } = event;
+        const { transcript, confidence } = results[resultIndex][0];
+
+        console.log(syntax(transcript));
+    });
+}
